c470d9ca2cbaa77c2ec1a7a0a736595184982d1d
James Titus <jamesttitusjr@outlook.com>
2022-08-31 11:17:08 -0400

    Add project files.

---
+++ GeometricShapes/Quad.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricShapes
+{
+    internal class Quad
+    {
+        public int Side1 { get; set; }
+        public int Side2 { get; set; }
+        public int Side3 { get; set; }
+        public int Side4 { get; set; }
+
+    }
+}
